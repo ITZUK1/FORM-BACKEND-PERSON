1454e3a8a0a56386daa4e154a9059b4098edbf47
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-03 12:22:41 -0500

    aparece una animacion de llenar todos los campos

--- Person-Form/src/app/components/user-form/user-form.component.ts
+++ Person-Form/src/app/components/user-form/user-form.component.ts
@@ -4,6 +4,7 @@ import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
   user: any = {
@@ -14,13 +15,32 @@ export class UserFormComponent {
     telefono: '',
     direccion: ''
   };
+  message: string = '';
+  isSuccess: boolean = false;
 
   constructor(private dataService: DataService) {}
 
   onSubmit() {
-    this.dataService.addUser(this.user).subscribe((response: any) => {
-      console.log('Usuario creado:', response);
-      // Aquí puedes reiniciar el formulario o hacer otras acciones
-    });
-  }
+    // Verificamos si todos los campos están completos
+    if (this.user.nombre && this.user.apellido && this.user.edad && this.user.correo && this.user.telefono && this.user.direccion) {
+        // Todos los datos están completos
+        this.dataService.addUser(this.user).subscribe(
+            (response: any) => {
+                console.log('Usuario creado:', response);
+                this.message = 'Los datos se guardaron correctamente.';
+                this.isSuccess = true;
+                // Aquí puedes reiniciar el formulario o hacer otras acciones
+            },
+            (error) => {
+                console.error('Error al crear el usuario:', error);
+                this.message = 'Ha ocurrido un error al guardar los datos.';
+                this.isSuccess = false;
+            }
+        );
+    } else {
+        // Datos faltantes
+        this.message = 'Por favor, complete todos los campos requeridos.';
+        this.isSuccess = false;
+    }
+}
 }
