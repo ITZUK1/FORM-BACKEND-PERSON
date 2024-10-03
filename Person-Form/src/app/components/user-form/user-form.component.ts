178dac07dea1444508de1999de91a308408671b5
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-03 13:57:04 -0500

    se creo la funcion de poder validar si tienen numeros o caraceres especiales

--- Person-Form/src/app/components/user-form/user-form.component.ts
+++ Person-Form/src/app/components/user-form/user-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
-
+ 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
@@ -17,30 +17,59 @@ export class UserFormComponent {
   };
   message: string = '';
   isSuccess: boolean = false;
-
+ 
   constructor(private dataService: DataService) {}
-
+ 
+  // Validar solo letras y espacios
+  validateName(event: any) {
+    const input = event.target.value;
+    event.target.value = input.replace(/[^a-zA-ZÀ-ÿ\s]/g, ''); // Eliminar todo lo que no sea letra o espacio
+  }
+ 
+  validateLastName(event: any) {
+    const input = event.target.value;
+    event.target.value = input.replace(/[^a-zA-ZÀ-ÿ\s]/g, ''); // Eliminar todo lo que no sea letra o espacio
+  }
+ 
+  // Validar si los campos de nombre y apellido contienen solo letras y espacios
+  isValidName(name: string): boolean {
+    return /^[a-zA-ZÀ-ÿ\s]+$/.test(name); // Expresión regular para letras y espacios
+  }
+ 
   onSubmit() {
+    // Verificamos si los campos contienen solo letras y espacios
+    if (!this.isValidName(this.user.nombre)) {
+      this.message = 'El nombre contiene caracteres inválidos.';
+      this.isSuccess = false;
+      return;
+    }
+ 
+    if (!this.isValidName(this.user.apellido)) {
+      this.message = 'El apellido contiene caracteres inválidos.';
+      this.isSuccess = false;
+      return;
+    }
+ 
     // Verificamos si todos los campos están completos
     if (this.user.nombre && this.user.apellido && this.user.edad && this.user.correo && this.user.telefono && this.user.direccion) {
-        // Todos los datos están completos
-        this.dataService.addUser(this.user).subscribe(
-            (response: any) => {
-                console.log('Usuario creado:', response);
-                this.message = 'Los datos se guardaron correctamente.';
-                this.isSuccess = true;
-                // Aquí puedes reiniciar el formulario o hacer otras acciones
-            },
-            (error) => {
-                console.error('Error al crear el usuario:', error);
-                this.message = 'Ha ocurrido un error al guardar los datos.';
-                this.isSuccess = false;
-            }
-        );
+      // Todos los datos están completos, enviamos al servidor
+      this.dataService.addUser(this.user).subscribe(
+        (response: any) => {
+          console.log('Usuario creado:', response);
+          this.message = 'Los datos se guardaron correctamente.';
+          this.isSuccess = true;
+          // Aquí puedes reiniciar el formulario o hacer otras acciones
+        },
+        (error) => {
+          console.error('Error al crear el usuario:', error);
+          this.message = 'Ha ocurrido un error al guardar los datos.';
+          this.isSuccess = false;
+        }
+      );
     } else {
-        // Datos faltantes
-        this.message = 'Por favor, complete todos los campos requeridos.';
-        this.isSuccess = false;
+      // Datos faltantes
+      this.message = 'Por favor, complete todos los campos requeridos.';
+      this.isSuccess = false;
     }
-}
+  }
 }
