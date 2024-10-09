11f4a30b6b06ef5aca41b12d252644f0d70c8246
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-09 06:28:39 -0500

    se elimino comentarios inecesarios

--- Person-Form/src/app/components/user.module.ts
+++ Person-Form/src/app/components/user.module.ts
@@ -1,9 +1,9 @@
-// user.module.ts
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms'; // Importar ReactiveFormsModule
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // Importa NgbModule para usar los modales
-import { UserListComponent } from './user-list/user-list.component'; // Asegúrate de que la ruta sea correcta
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
@@ -11,11 +11,11 @@ import { UserListComponent } from './user-list/user-list.component'; // Asegúra
   ],
   imports: [
     CommonModule, // Necesario para directivas comunes
-    ReactiveFormsModule, // Asegúrate de incluir esto
+    ReactiveFormsModule, 
     NgbModule, // Para usar NgbModal
   ],
   exports: [
-    UserListComponent, // Exporta si lo vas a utilizar en otros módulos
+    UserListComponent, 
   ]
 })
 export class UserModule {}
